c74b12594e7ded8a3f4cd3fb0d565b77025e2693
jack <jack>
2020-02-05 20:39:11 +0800

    delete unused

--- src/main/java/com/jack/store/web/rest/CategoryResource.java
+++ src/main/java/com/jack/store/web/rest/CategoryResource.java
@@ -7,7 +7,6 @@ import com.jack.store.service.CategoryService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
